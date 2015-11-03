648b70b5d94425c598d389848b55a11a63fcd348
Briceno, Joaquin <joaquin.briceno@itg.com>
2015-11-03 12:15:12 -0800

    Modified a test vie.'w

--- Mastering Web App Development with AngularJS/1820OS_11_Code/01 - inner working - how digest loop works/internals.js
+++ Mastering Web App Development with AngularJS/1820OS_11_Code/01 - inner working - how digest loop works/internals.js
@@ -1,30 +1,23 @@
-angular.module('internals', [])
-  .directive('simpleModel', function ($parse) {
+angular.module('internals', []).directive('simpleModel', function ($parse) {
     return function (scope, element, attrs) {
-
-      var modelGetter = $parse(attrs.simpleModel);
-      var modelSetter = modelGetter.assign;
-
-      //Model -> DOM updates
-      scope.$watch(modelGetter, function(newVal, oldVal){
-        element.val(newVal);
-      });
-
-      //DOM -> Model updates
-      element.bind('input', function () {
-        scope.$apply(function () {
-          modelSetter(scope, element.val());
+        var modelGetter = $parse(attrs.simpleModel);
+        var modelSetter = modelGetter.assign;
+        //Model -> DOM updates
+        scope.$watch(modelGetter, function(newVal, oldVal){
+            element.val(newVal);
         });
-      });
+        //DOM -> Model updates
+        element.bind('input', function () {
+            scope.$apply(function () {
+                modelSetter(scope, element.val());
+            });
+        });
     };
-  })
-
-  .directive('simpleBind', function ($parse) {
+}).directive('simpleBind', function ($parse) {
     return function (scope, element, attrs) {
-
-      var modelGetter = $parse(attrs.simpleBind);
-      scope.$watch(modelGetter, function(newVal, oldVal){
-        element.text(newVal);
-      });
+        var modelGetter = $parse(attrs.simpleBind);
+        scope.$watch(modelGetter, function(newVal, oldVal){
+            element.text(newVal);
+        });
     };
-  });
+});
